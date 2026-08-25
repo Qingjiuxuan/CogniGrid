68edec66a5fe004e65da776b0c1a6fcbb9ac2993
Qingjiuxuan <97121103+Qingjiuxuan@users.noreply.github.com>
2026-08-25 09:15:31 +0800

    Add files via upload

---
+++ src/assets/index.ts
@@ -0,0 +1,26 @@
+// Direct JPG imports of user provided executive and brand assets
+import brandLogoJpg from './images/cognigrid_brand_logo_1787616403683.jpg';
+import ethanJpg from './images/exec_ethan_ceo_1787616046363.jpg';
+import lucasJpg from './images/exec_lucas_coo_1787616062294.jpg';
+import julianJpg from './images/exec_julian_cto_1787616075055.jpg';
+import arthurJpg from './images/exec_arthur_cfo_1787616090941.jpg';
+import stellaJpg from './images/exec_stella_cmo_1787616105563.jpg';
+import milesJpg from './images/exec_miles_cgo_1787616120147.jpg';
+import irisJpg from './images/exec_iris_cbdo_1787616135363.jpg';
+
+export const BrandLogos = {
+  sm: brandLogoJpg,
+  md: brandLogoJpg,
+  lg: brandLogoJpg,
+  default: brandLogoJpg,
+};
+
+export const ExecutiveImages = {
+  ethan: ethanJpg,
+  lucas: lucasJpg,
+  julian: julianJpg,
+  arthur: arthurJpg,
+  stella: stellaJpg,
+  miles: milesJpg,
+  iris: irisJpg,
+};
